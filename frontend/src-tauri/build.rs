df3078dfc66fd6079a391f107ffb40d94bee94f7
BButner <butner.beau@gmail.com>
2023-04-13 16:44:32 -0500

    More frontend base work

--- frontend/src-tauri/build.rs
+++ frontend/src-tauri/build.rs
@@ -1,3 +1,17 @@
-fn main() {
-  tauri_build::build()
+use std::path::PathBuf;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut protos_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    protos_path.pop();
+    protos_path.pop();
+    protos_path.push("protos");
+
+    tonic_build::configure()
+        .build_server(false)
+        .build_client(true)
+        .compile(&[protos_path.join("fence.proto").as_path()], &[protos_path])?;
+
+    tauri_build::build();
+
+    Ok(())
 }

0439387073ffd510ea76108cd2c3915ed9f175ad
BButner <butner.beau@gmail.com>
2023-04-25 13:12:10 -0500

    jk we're actually using Svelte now

--- frontend/lib/connection-helper.ts
+++
@@ -1,5 +0,0 @@
-import { invoke } from "./tauri"
-
-export const connectToGrpc = async (hostname: string) => {
-  await invoke("connect_grpc", { hostname })
-}
--- frontend/lib/state/displays.ts
+++
@@ -1,5 +0,0 @@
-import { atom } from "jotai"
-
-import { IDisplay } from "../displays-helper"
-
-export const displaysAtom = atom<IDisplay[]>([])

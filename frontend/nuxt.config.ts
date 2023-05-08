32b5548e304262626679d456d7b67fde61bd20a3
BButner <butner.beau@gmail.com>
2023-05-08 17:46:27 -0500

    Adds tailwind

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,4 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  css: ["~/assets/css/tailwind.scss"],
   ssr: false,
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
 })
